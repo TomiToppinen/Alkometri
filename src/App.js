461dae92f92db64d070e5b0b70e81ca57c1bda6b
Tomi Toppinen <n0toto00@students.oamk.fi>
2021-09-17 05:45:47 +0300

    alkometri

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [bottle, setBottle] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState(0)
+  const [blood, setBlood] = useState(0)
+  
+  function calculate(e) {
+    e.preventDefault();
+    let tulos = 0;
+    if (gender === 'male') {
+      tulos = (bottle * 0.70)
+    }
+    else {
+      tulos = (bottle * 0.60)
+    }
+    setBlood(tulos);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <form onSubmit={calculate}> 
+     <h3>Calculating alcohol blood level</h3>
+     <div>
+       <label>Weight</label>
+       <input name ="weight" type="number" step="0.01"
+        onChange={e => setWeight(e.target.value)}
+        value={weight} />
     </div>
+    <div>
+      <label>Bottles</label>
+      <input type ="number" step="1"
+        onChange={e => setBottle(e.target.value)} 
+        value={bottle} />
+    </div>
+    <div>
+      <label>Time</label>
+      <input type ="number" step="1"
+        onChange={e => setTime(e.target.value)}
+        value={time} />
+    </div>
+    <div>
+      <label>Gender</label>
+      <input type ="radio" name="gender" value="male"
+        onChange={e => setGender(e.target.value)}/>
+      <label>Male</label>
+      <input type ="radio" name="gender" value="female"
+        onChange={e => setGender(e.target.value)}/> 
+        <label>Female</label> 
+    </div>
+    <div>
+      <label>Blood level</label>
+      <output>{blood}</output>
+    </div>
+    <div>
+      <button>Calculate</button>
+    </div>
+    
+
+   </form>
   );
 }
 
